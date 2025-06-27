de0a5dc8c6d979de6e031a466603b2a2d9c76bfd
E Sasi Kiran <sasikiranessamaraju@gmail.com>
2025-06-27 20:46:18 +0530

    Setup React and Parcel

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <div>Hello React</div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
